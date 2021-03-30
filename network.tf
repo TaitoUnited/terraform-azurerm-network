baa9a12761d64b079d392fa58feb609d1a5330dc
keskiju <jukka.keskiluopa@taitounited.fi>
2021-03-30 14:10:39 +0300

    feat: rename virtual network

--- network.tf
+++ network.tf
@@ -15,7 +15,7 @@
  */
 
 resource "azurerm_virtual_network" "vnet" {
-  name                = var.name
+  name                = "${var.name}-vnet"
   address_space       = ["10.1.0.0/16"]
   location            = var.location
   resource_group_name = var.resource_group_name
