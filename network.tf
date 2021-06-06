ff0fa2f2d9248cef2d074d1ba2acdb024332ff91
Jukka Keski-Luopa <jukka.keskiluopa@taitounited.fi>
2021-06-06 22:12:46 +0300

    feat: change vnet address_space, add name outputs

--- network.tf
+++ network.tf
@@ -16,7 +16,7 @@
 
 resource "azurerm_virtual_network" "vnet" {
   name                = "${var.name}-vnet"
-  address_space       = ["10.1.0.0/16"]
+  address_space       = ["10.0.0.0/12"]
   location            = var.location
   resource_group_name = var.resource_group_name
 }
--- outputs.tf
+++ outputs.tf
@@ -18,8 +18,16 @@ output "virtual_network_id" {
   value = azurerm_virtual_network.vnet.id
 }
 
+output "virtual_network_name" {
+  value = azurerm_virtual_network.vnet.name
+}
+
 output "subnet_id" {
   value = azurerm_subnet.subnet.id
+}
+
+output "subnet_name" {
+  value = azurerm_subnet.subnet.name
 }
 
 /* TODO: enable
